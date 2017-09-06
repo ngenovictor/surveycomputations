2713180f2d25432712b57ca365ae82d4564dcc68
Victor Ngeno <ngenovictor321@gmail.com>
2017-09-06 21:57:25 +0300

    create option for extra infinite input forms for coordinates and methods in views

--- app/views.py
+++ app/views.py
@@ -14,11 +14,18 @@ def index(request):
 def compute_area(request):
     template = "compute_area.html"
     title = "Area Result"
-    point1 = (int(request.GET.get("coordinateX1")), int(request.GET.get("coordinateY1")))
-    point2 = (int(request.GET.get("coordinateX2")), int(request.GET.get("coordinateY2")))
-    point3 = (int(request.GET.get("coordinateX3")), int(request.GET.get("coordinateY3")))
+    number_of_points = int(request.GET.get("point_number"))
+    coordinates = []
+    for i in range(number_of_points):
+        x_string = "coordinateX" + str(i+1)
+        print(x_string)
+        y_string = "coordinateY" + str(i+1)
+        print(y_string)
+        point = (int(request.GET.get(x_string)), int(request.GET.get(y_string)))
+        coordinates.append(point)
+
     precision = int(request.GET.get("precision"))
-    coordinates = [point1, point2, point3]
+
     area = AreaCalculator().area_with_coordinates(coordinates, precision)
     context = {
         "title": title,
