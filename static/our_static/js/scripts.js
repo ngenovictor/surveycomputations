80bdf3a11faa16517c5ad441369e484bbd2d2881
Victor Ngeno <ngenovictor321@gmail.com>
2017-09-09 13:06:14 +0300

    update script with better request options

--- static/our_static/js/scripts.js
+++ static/our_static/js/scripts.js
@@ -23,15 +23,18 @@ $(document).ready(function(){
     $("form#feedback_form").submit(function (event) {
         event.preventDefault();
         var name = $("input#feed_back_name").val();
+        
         var email = $("input#feed_back_email").val();
+
         var message = $("textarea#feed_back_message").val();
+
 
         var feedback_url = $("input#feedback_url").val();
 
         $("#feed_back_inputs").modal("toggle");
 
         $.ajax({
-            url:feedback_url+"?name=name&&email=email&&message=message",
+            url:feedback_url+"?name="+name+"&&email="+email+"&&message="+message,
             success:function(data){
                 console.log(data);
                 $("#feedback_message").text(data["message"]);
