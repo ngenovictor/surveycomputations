1e302a419de76af3376fbefd27c9c9920d9bc7a9
Victor Ngeno <ngenovictor321@gmail.com>
2017-09-09 13:10:37 +0300

    update static files

--- static/static_root/js/scripts.js
+++ static/static_root/js/scripts.js
@@ -8,16 +8,38 @@ $(document).ready(function(){
             "<div class='form-group'>"+
                 "<div  class='input-group'>" +
                     "<div class='input-group-addon'>X"+point_numbers+"</div>" +
-                    "<input type='number' id='coordinateX"+point_numbers+"' name='coordinateX"+point_numbers+"' class='form-control'/>"+
+                    "<input type='number' id='coordinateX"+point_numbers+"' name='coordinateX"+point_numbers+"' class='form-control' required/>"+
                 "</div>"+
                 "<div  class='input-group'>" +
                     "<div class='input-group-addon'>Y"+point_numbers+"</div>" +
-                    "<input type='number' id='coordinateY"+point_numbers+"' name='coordinateY"+point_numbers+"' class='form-control'/>"+
+                    "<input type='number' id='coordinateY"+point_numbers+"' name='coordinateY"+point_numbers+"' class='form-control' required/>"+
                 "</div>"+
             "</div>"
         );
         $("input#point_number_input").attr("value", point_numbers.toString());
         $("span#point_number").text(point_numbers);
 
+    });
+    $("form#feedback_form").submit(function (event) {
+        event.preventDefault();
+        var name = $("input#feed_back_name").val();
+
+        var email = $("input#feed_back_email").val();
+
+        var message = $("textarea#feed_back_message").val();
+
+
+        var feedback_url = $("input#feedback_url").val();
+
+        $("#feed_back_inputs").modal("toggle");
+
+        $.ajax({
+            url:feedback_url+"?name="+name+"&&email="+email+"&&message="+message,
+            success:function(data){
+                console.log(data);
+                $("#feedback_message").text(data["message"]);
+                $("#thank_you_feedback").modal("toggle");
+            }
+        })
     })
 });
