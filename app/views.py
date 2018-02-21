1dae39c465d1335fcdd9b32a7a8d59e8a4234f15
Victor Ngeno <victor.ngeno@gro-intelligence.com>
2018-02-21 11:55:16 +0300

    put compute area on its own

--- app/views.py
+++ app/views.py
@@ -45,18 +45,21 @@ def give_feedback(request):
 def compute_area(request):
     template = "compute_area.html"
     title = "Area Result"
-    number_of_points = int(request.GET.get("point_number"))
-    coordinates = []
-    for i in range(number_of_points):
-        x_string = "coordinateX" + str(i+1)
-        y_string = "coordinateY" + str(i+1)
+    area = None
+    coordinates = None
+    if request.method == 'POST':
+        number_of_points = int(request.POST.get("point_number"))
+        coordinates = []
+        for i in range(number_of_points):
+            x_string = "coordinateX" + str(i+1)
+            y_string = "coordinateY" + str(i+1)
 
-        point = (int(request.GET.get(x_string)), int(request.GET.get(y_string)))
-        coordinates.append(point)
+            point = (int(request.POST.get(x_string)), int(request.POST.get(y_string)))
+            coordinates.append(point)
 
-    precision = int(request.GET.get("precision"))
+        precision = int(request.POST.get("precision"))
 
-    area = AreaCalculator().area_with_coordinates(coordinates, precision)
+        area = AreaCalculator().area_with_coordinates(coordinates, precision)
     context = {
         "title": title,
         "area": area,
