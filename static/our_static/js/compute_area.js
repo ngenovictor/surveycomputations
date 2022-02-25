a01802e90d5aafa432a1757cc505af93c1620704
Victor Ngeno <ngenovictor321@gmail.com>
2022-02-26 00:53:15 +0300

    start to work on mapping elements

--- static/our_static/js/compute_area.js
+++ static/our_static/js/compute_area.js
@@ -1,42 +1,35 @@
-
-$(document).ready(function(){
-    var point_numbers = 3;
-    $("#add-coordinate-inputs").click(function (event) {
-        event.preventDefault();
-        point_numbers+=1;
-        console.log(point_numbers);
-        $("#point-inputs").append(
-            "<div class='form-group'>"+
-                "<div  class='input-group'>" +
-                    "<div class='input-group-addon'>X"+point_numbers+"</div>" +
-                    "<input type='number' id='coordinateX"+point_numbers+"' name='coordinateX"+point_numbers+"' class='form-control' required/>"+
-                "</div>"+
-                "<div  class='input-group'>" +
-                    "<div class='input-group-addon'>Y"+point_numbers+"</div>" +
-                    "<input type='number' id='coordinateY"+point_numbers+"' name='coordinateY"+point_numbers+"' class='form-control' required/>"+
-                "</div>"+
-            "</div>"
-        );
-        $("input#point_number_input").attr("value", point_numbers.toString());
-        $("span#point_number").text(point_numbers);
-    });
-    var osmlayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+function createMap() {
+    var osmlayer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoibmdlbm92aWN0b3IzMjEiLCJhIjoiY2wwMndqd3VmMDFtYTNrcG93ZjRxN285eCJ9.-Qd_gd0MTNBW2jq03h1ivg', {
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
         '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
         'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-        id: 'mapbox.satellite'
-    });
-    var none_layer = L.tileLayer('', {
-        maxZoom: 18,
-        attribution: 'Empty'
+        id: 'mapbox/satellite-streets-v11'
     });
     var map = L.map('map', {
         center: [-1.0500, 37.0833],
         zoom: 10,
         minZoom: 2,
         maxZoom: 18,
-        zoomControl: false,
+        zoomControl: true,
         layers: [osmlayer,]
     });
+    var startPickingbutton = L.Control.extend({
+        options: {
+            position: 'bottomleft',
+        },
+        onAdd: function(map){
+            var button = L.DomUtil.create("button", "btn btn-primary");
+            button.innerHTML = "Start Picking";
+            // button.style.width = "100px"
+            button.onclick = function(){
+                console.log('buttonClicked');
+            }
+            return button;
+        }
+    });
+    map.addControl(new startPickingbutton());
+}
+$(document).ready(function(){
+    createMap();
 });
