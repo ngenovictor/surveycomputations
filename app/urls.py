03e5167eafb8a1f12836020450fae410a6ec9476
Victor Ngeno <ngenovictor321@gmail.com>
2022-02-25 22:18:11 +0300

    upgrade libs

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    url(r'^$', views.index, name="home"),
-    url(r'^about', views.about, name="about"),
-    url(r'^compute_area', views.compute_area, name="compute_area"),
-    url(r'^give_feedback', views.give_feedback, name="give_feedback"),
+    path('', views.index, name="home"),
+    path('about', views.about, name="about"),
+    path('compute_area', views.compute_area, name="compute_area"),
+    path('give_feedback', views.give_feedback, name="give_feedback"),
 ]
--- surveytools/wsgi.py
+++ surveytools/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for survey project.
+WSGI config for surveytools project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "survey.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'surveytools.settings')
 
 application = get_wsgi_application()
